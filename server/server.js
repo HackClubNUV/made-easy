bd934d89ac54dbf96b4000823ccb7e4317dca8c4
nimit2801 <mnimitsavant@outlook.com>
2021-07-27 20:29:09 +0530

    EJS Quick Fix in backend ✅

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
 const app = express();
-const cors = require('cors');
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
