c6cf435078f42b6ae267e4a450f102653851cd3e
TtvTooSmart <20124015@gmail.com>
2022-09-02 23:53:36 +0530

    updated link

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ import auth from '../helper/auth';
 
 const discordAuth = () => {
   window.open(
-    `https://discord.com/api/oauth2/authorize?client_id=1002481981281534002&redirect_uri=https%3A%2F%2Fgdscnuv-applications.herokuapp.com%2Fapply&response_type=code&scope=identify`,
+    `https://discord.com/api/oauth2/authorize?client_id=1002481981281534002&redirect_uri=https%3A%2F%2Fgdscnuv-applications.herokuapp.com%2Fapply&response_type=token&scope=identify`,
     '_parent'
   );
   auth.login();
@@ -120,3 +120,5 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+
