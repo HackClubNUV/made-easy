6d401e43a34b10376e6def2e0c754e28843600ac
nimit2801 <mnimitsavant@outlook.com>
2022-09-07 00:07:43 +0530

    test 1

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ import auth from '../helper/auth';
 
 const discordAuth = () => {
   window.open(
-    `https://discord.com/api/oauth2/authorize?client_id=1002481981281534002&redirect_uri=https%3A%2F%2Fgdscnuv-applications.herokuapp.com%2Fapply&response_type=token&scope=identify`,
+    `https://discord.com/api/oauth2/authorize?client_id=871148961191297074&redirect_uri=https%3A%2F%2Fgdscnuv.herokuapp.com%2Fapply&response_type=token&scope=identify`,
     '_parent'
   );
   auth.login();
@@ -120,5 +120,3 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
-
-
